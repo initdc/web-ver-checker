c92e6a9e9f82730d2950561e6d7b6b35da96c9f1
initdc <initd@outlook.com>
2021-06-29 17:59:32 +0800

    [dev.06.29] optional function for get origin uri

--- main.ts
+++ main.ts
@@ -7,17 +7,30 @@ import {
   yellow,
 } from "https://deno.land/std@0.99.0/fmt/colors.ts";
 
-// async function get_origin() {
-const res = await fetch("https://api.kodcloud.com/?app%2Fversion");
+const kodbox_uri = "https://api.kodcloud.com/?app%2Fversion";
 
-if (res.ok) {
-  const resObj = await res.json();
-  console.log(green("Success"));
-  console.log(resObj.data.server.version);
-} else {
-  const err = await res.text();
-  console.error("Failure to GET", err);
+enum Mode {
+  object,
+  regexp
 }
-// }
 
-// get_origin
+interface Options {
+  object: Array<string>;
+  regexp?: RegExp;
+}
+
+// , mode: Mode, options: Options
+async function get_origin(uri: string, options: Options) {
+  const res = await fetch(uri);
+
+  if (res.ok) {
+    const resObj = await res.json();
+    console.log(green("Success"));
+    console.log(resObj[options.object[0]]);
+  } else {
+    const err = await res.text();
+    console.error(red("Failure to GET\n"), err);
+  }
+}
+
+get_origin(kodbox_uri, {object: ['data', 'server', 'version']});
